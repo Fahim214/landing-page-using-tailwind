04fbb40affd9b427158c091ff5c8461ea57faa93
Fahim214 <fahimtu456@gmail.com>
2022-04-01 19:06:18 +0700

    update comp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Support from "./components/Support";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <About />
+      <Support />
+    </div>
+  );
+}
+
+export default App;
